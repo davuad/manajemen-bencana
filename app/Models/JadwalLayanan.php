478589439c3f1cff29a53190081b119513fe7dbc
faoziah28 <anisatunfaoziah@gmail.com>
2026-06-04 20:44:35 +0700

    Tambah model JadwalLayanan

---
+++ app/Models/JadwalLayanan.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JadwalLayanan extends Model
+{
+    protected $table = 'jadwal';
+
+    protected $fillable = [
+        'bencana_id',
+        'pegawai_id',
+        'tanggal_layanan',
+        'jam_mulai',
+        'jam_selesai',
+        'jenis_layanan',
+        'sarana',
+        'petugas_lapangan',
+        'lokasi_layanan',
+        'status'
+    ];
+
+    public function bencana()
+    {
+        return $this->belongsTo(Bencana::class);
+    }
+
+    public function pegawai()
+    {
+        return $this->belongsTo(Pegawai::class);
+    }
+}
