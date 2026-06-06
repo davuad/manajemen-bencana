74a947a60c1f372c3bde5a1ee3b655fff71287de
yusufefendi123 <yyyyuuuussssuuuuf@gmail.com>
2026-06-06 14:45:01 +0700

    Update Migrasion Pengaduan

--- database/migrations/2000_00_00_000000_create_kategori_bencana_table.php
+++ database/migrations/2000_00_00_000000_create_kategori_bencana_table.php
@@ -1,4 +1,4 @@
-<?php
+2<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
--- database/migrations/2001_01_01_000001_foto_pengaduan.php
+++ database/migrations/2001_01_01_000001_foto_pengaduan.php
@@ -11,7 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('foto_pengaduan', function (Blueprint $table) {
+            $table->id('id');
+
+            $table->foreignId('pengaduan_bencana_id')
+                ->constrained('pengaduan_bencana')
+                ->onDelete('cascade');
+
+            $table->string('file_foto');
+            $table->string('keterangan', 100)->nullable();
+
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('foto_pengaduan');
     }
 };
