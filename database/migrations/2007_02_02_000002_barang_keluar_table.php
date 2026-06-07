12d0c225b4551c54aa93c839639bbb6c3353c3fa
M Arfazzuhaili <arfaprapen@gmail.com>
2026-06-07 15:24:28 +0700

    update barang_keluar table migration

--- database/migrations/2007_02_02_000002_barang_keluar_table.php
+++ database/migrations/2007_02_02_000002_barang_keluar_table.php
@@ -6,19 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('barang_keluar', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('gudang_id')->constrained('gudang')->cascadeOnDelete();
+            $table->foreignId('pengajuan_barang_id')->constrained('pengajuan_barang')->cascadeOnDelete();
+            $table->foreignId('petugas_gudang_id')->constrained('pegawai')->cascadeOnDelete();
+            $table->foreignId('updated_by')->nullable()->constrained('user')->nullOnDelete();
+
+            $table->date('tgl_keluar');
+            $table->enum('status_proses', ['diproses', 'dikirim', 'selesai', 'dibatalkan']);
+            $table->string('catatan')->nullable();
+
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('barang_keluar');
     }
 };
