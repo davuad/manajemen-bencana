0c7c187da7cdf9c8fe7303b6a72cff20d7186243
ratnaanyh <ratnanurhidayah475@gmail.com>
2026-06-07 19:23:16 +0700

    menambah migrasi penerima distribusi

--- database/migrations/2004_00_00_000000_penerimaan_distribusi.php
+++ database/migrations/2004_00_00_000000_penerimaan_distribusi.php
@@ -11,7 +11,24 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+         Schema::create('penerima_distribusi', function (Blueprint $table) {
+
+            $table->id('penerima_id');
+
+            $table->foreignId('detail_distribusi_id')
+                    ->constrained('detail_distribusi')
+                    ->onDelete('cascade');
+
+            $table->string('nama_penerima', 100);
+            $table->string('jabatan', 100);
+            $table->string('instansi', 100);
+            $table->string('alamat', 150);
+            $table->string('no_hp', 15);
+
+            $table->string('status', 100);
+
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +36,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('penerima_distribusi');
     }
 };
