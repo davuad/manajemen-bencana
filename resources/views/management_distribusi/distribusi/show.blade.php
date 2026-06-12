ca12ef8646368aa115f4de010dcfde92196b071b
yunisetianingsih <yunisetianingsih2005@gmail.com>
2026-06-12 14:23:40 +0700

    Memperbaiki route distribusi

--- resources/views/management_distribusi/distribusi/show.blade.php
+++ resources/views/management_distribusi/distribusi/show.blade.php
@@ -148,7 +148,7 @@
 
     <!-- BUTTON -->
     <div class="flex justify-end gap-3 mt-6">
-        <a href="{{ route('management_distribusi.distribusi.index') }}"
+        <a href="{{ route('admin.management_distribusi.distribusi.index') }}"
            class="px-4 py-2 bg-gray-300 rounded-lg">
             Kembali
         </a>
